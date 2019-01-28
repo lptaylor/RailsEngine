ca21b69c0331f60c669a6ba26e913511aef79373
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-27 19:46:20 -0700

    Final commit

--- app/controllers/api/v1/merchants/search_functions_controller.rb
+++ app/controllers/api/v1/merchants/search_functions_controller.rb
@@ -1,24 +1,15 @@
-class API::V1::Merchants::SearchFunctionsController < ApplicationController
+class Api::V1::Merchants::SearchFunctionsController < ApplicationController
   def index
-
+    render json: MerchantSerializer.new(Merchant.where(search_params))
   end
 
   def show
-    render json: MerchantSerializer.new(search_params(params))
+    render json: MerchantSerializer.new(Merchant.find_by(search_params))
   end
 
   private
 
-  def search_params(params)
-    case params
-      when params[:id]
-        Merchant.where(id: params[:id])
-      when params[:name]
-        Merchant.where(name: params[:name])
-      when params[:created_at]
-        Merchant.where(created_at: params[:created_at])
-      when params[:updated_at]
-        Merchant.where(updated_at: parmas[:updated_at])
-    end
+  def search_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 end
