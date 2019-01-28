72778453cb86cf046d59b796628e5e31dc16f222
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-27 19:44:59 -0700

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1  do
-      resources :merchants, only: [:index, :show]
       namespace :merchants do
         get 'find', to: 'search_functions#show'
+        get 'find_all', to: 'search_functions#index'
+        get 'random', to: 'random#show'
+        get 'most_revenue', to: 'most_revenue#index'
+      end
+      resources :merchants, only: [:index, :show] do
+        get 'items', to: 'items#index'
+        get 'invoices', to: 'invoices#index'
       end
     end
   end
